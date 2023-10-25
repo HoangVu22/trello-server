ad3b065ed877d1e502e1e1121345cfb678092013
Nguyễn Hoàng Vũ <hoangvu22062001@gmail.com>
2023-10-25 14:52:20 +0700

    Writing APIs with Express Router

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,1 +1,16 @@
-// index
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { boardRoutes } from './boardRoutes'
+
+const router = express.Router()
+
+// Check APIs v1/status
+router.get('/status', (req, res) => {
+  res.status(StatusCodes.OK).json({ message: 'APIs v1 are ready to use' })
+})
+
+// Boards APIs
+router.use('/boards', boardRoutes)
+
+export const APIs_V1 = router
+
--- src/server.js
+++ src/server.js
@@ -2,13 +2,12 @@ import express from 'express'
 import exitHook from 'async-exit-hook'
 import { connectDB, closeDB } from '~/config/mongodb'
 import { env } from '~/config/environment'
+import { APIs_V1 } from '~/routes/v1'
 
 const START_SERVER = () => {
   const app = express()
   
-  app.get('/', (req, res) => {
-    res.end('<h1>Hello World!</h1><hr>')
-  })
+  app.use('/v1', APIs_V1)
   
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     console.log(`🚀🚀 Hi ${env.AUTHOR}, Server ready at http://${env.APP_HOST}:${env.APP_PORT}/`)
